5eb82e1c2a314d6fc7ed673277fe7cbacb4a2211
Takue-10 <takudzwamaseva020@gmail.com>
2024-05-08 00:38:41 +0200

    Shortest Job First Implementation

--- src/barScheduling/Barman.java
+++ src/barScheduling/Barman.java
@@ -1,59 +1,94 @@
 package barScheduling;
-
-import java.util.Random;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import java.util.Comparator;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.CountDownLatch;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 
 /*
  Barman Thread class.
  */
 
-public class Barman extends Thread {
-	
-
+public class Barman extends Thread 
+{
 	private CountDownLatch startSignal;
 	private BlockingQueue<DrinkOrder> orderQueue;
+	private List<DrinkOrder> roundRobinList;
+	private int schedAlg;
 
-
-	
-	Barman(  CountDownLatch startSignal,int schedAlg) {
-		if (schedAlg==0)
+	Barman(  CountDownLatch startSignal,int schedAlg) 
+	{
+		this.schedAlg = schedAlg;
+		// First Come First Serve
+		if (schedAlg == 0) 
+		{
 			this.orderQueue = new LinkedBlockingQueue<>();
-		//FIX below
-		else this.orderQueue = new LinkedBlockingQueue<>(); //this just does the same thing 
-		
+		}
+		// Shortest Job First
+		else if (schedAlg == 1)
+		{
+			this.orderQueue = new PriorityBlockingQueue<>(11, Comparator.comparingInt(DrinkOrder::getExecutionTime));
+		} 
+		// Round Robin
+		else if (schedAlg == 2) 
+		{
+			this.roundRobinList = new LinkedList<>();
+			this.orderQueue = new LinkedBlockingQueue<>();
+		}
+
 	    this.startSignal=startSignal;
 	}
 	
-	
-	public void placeDrinkOrder(DrinkOrder order) throws InterruptedException {
-        orderQueue.put(order);
+	public void placeDrinkOrder(DrinkOrder order) throws InterruptedException 
+	{
+		if (schedAlg == 2 ) 
+		{
+			synchronized (roundRobinList)
+			{
+				roundRobinList.add(order);
+            }
+		} else
+		{
+			orderQueue.put(order);
+		}
     }
 	
-	
-	public void run() {
-		try {
-			DrinkOrder nextOrder;
-			
+	public void run()
+	 {
+		try
+		 {
+
 			startSignal.countDown(); //barman ready
 			startSignal.await(); //check latch - don't start until told to do so
 
-			while(true) {
-				nextOrder=orderQueue.take();
-				System.out.println("---Barman preparing order for patron "+ nextOrder.toString());
-				sleep(nextOrder.getExecutionTime()); //processing order
-				System.out.println("---Barman has made order for patron "+ nextOrder.toString());
-				nextOrder.orderDone();
-			}
+	        executeStandard(); // FCFS = 0, SJF = 1, RR = 2
 				
-		} catch (InterruptedException e1) {
+		} catch (InterruptedException e1) 
+
+		{
 			System.out.println("---Barman is packing up ");
 		}
+	}
+
+	private void executeStandard() throws InterruptedException 
+	{
+		DrinkOrder nextOrder;
+		while (true) 
+		{
+			nextOrder = orderQueue.take();
+			processOrder(nextOrder);
+		}
+	}
+
+	public void processOrder(DrinkOrder order) throws InterruptedException 
+	{
+		System.out.println("---Barman preparing order for patron "+ order.toString());
+		sleep(order.getExecutionTime()); //processing order
+		System.out.println("---Barman has made order for patron "+ order.toString());
+		order.orderDone();
 	}
 }
 

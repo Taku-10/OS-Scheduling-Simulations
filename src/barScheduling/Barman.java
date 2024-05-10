6c36169eae8548ddce56c219b6f647aab862b5ef
Takue-10 <takudzwamaseva020@gmail.com>
2024-05-10 03:33:41 +0200

    ModifyBramn class to to handle all 3 scheduling algorithms

--- src/barScheduling/Barman.java
+++ src/barScheduling/Barman.java
@@ -1,79 +1,147 @@
 package barScheduling;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.PriorityBlockingQueue;
-import java.util.concurrent.atomic.AtomicLong;
-import java.util.concurrent.CountDownLatch;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
-
-/*
- Barman Thread class.
- */
 
 public class Barman extends Thread 
 {
+
 	private CountDownLatch startSignal;
 	private BlockingQueue<DrinkOrder> orderQueue;
-	private List<DrinkOrder> roundRobinList;
-	private int schedAlg;
+	private Map<DrinkOrder, Integer> remainingTimes = new HashMap<>();
+    private int quantum = 25;  // Quantum time for Round Robin scheduling
+    private int schedAlg;  // Scheduling algorithm identifier 0=FCFS, 1=SJF, 2=RR
 
-	Barman(  CountDownLatch startSignal,int schedAlg) 
-	{
-		this.schedAlg = schedAlg;
-		// First Come First Serve
-		if (schedAlg == 0) 
+	Barman(  CountDownLatch startSignal,int schedAlg)
+	 {
+		if (schedAlg==0) // FCFS
 		{
 			this.orderQueue = new LinkedBlockingQueue<>();
-		}
-		// Shortest Job First
-		else if (schedAlg == 1)
-		{
-			this.orderQueue = new PriorityBlockingQueue<>(11, Comparator.comparingInt(DrinkOrder::getExecutionTime));
 		} 
-		
-	    this.startSignal = startSignal;
+		else if (schedAlg == 1) // SJF
+		{
+			this.orderQueue = new PriorityBlockingQueue<>(11, new Comparator<DrinkOrder>() 
+			{
+                @Override
+                public int compare(DrinkOrder o1, DrinkOrder o2)
+				{
+                    return Integer.compare(o1.getExecutionTime(), o2.getExecutionTime());
+                }
+            });
+		} 
+		else if (schedAlg == 2) // RR
+		{
+			this.orderQueue = new LinkedBlockingQueue<>();
+		} 
+		else 
+		{
+			throw new IllegalArgumentException("Unsupported scheduling algorithm");
+		}
+			
+	    this.startSignal=startSignal;
 	}
 	
-	public void placeDrinkOrder(DrinkOrder order) throws InterruptedException 
+/**
+ * The placeDrinkOrder method is responsible for adding a new drink order to the barman's order queue.
+ * It takes a DrinkOrder object as a parameter and adds it to the queue.
+ * If the scheduling algorithm is set to Round Robin (RR), the method also initializes the remaining time for the order.
+ * @param order The drink order to be added to the queue.
+ */
+	public void placeDrinkOrder(DrinkOrder order) throws InterruptedException
 	{
-		orderQueue.put(order);
+        orderQueue.put(order);
+		// Initialize remaining time for RR for tracking
+		if (schedAlg == 2) 
+		{ 
+            remainingTimes.put(order, order.getExecutionTime());  
+        }
     }
 	
-	public void run()
-	 {
-		try
-		 {
+/**
+ * The run method of the Barman thread.
+ * This method is responsible for processing drink orders based on the scheduling algorithm.
+ * It waits for the start signal, then continuously takes orders from the order queue,
+ * processes them according to the scheduling algorithm, and notifies the patrons when their orders are ready.
+ * If an InterruptedException occurs, it prints a message indicating that the barman is packing up.
+ */
+	public void run() 
+	{
+		try 
+		{
+			DrinkOrder nextOrder;
+			// Signal that the barman is ready
+			startSignal.countDown(); 
+			// Wait until the start signal is received
+			startSignal.await(); 
 
-			startSignal.countDown(); //barman ready
-			startSignal.await(); //check latch - don't start until told to do so
-
-	        executeStandard(); // FCFS = 0, SJF = 1, RR = 2
-		} 
-		catch (InterruptedException e1) 
-
+			while(true) 
+			{
+				// Take the next order from the queue
+				nextOrder=orderQueue.take();
+				// Process the order based on the scheduling algorithm
+				if (schedAlg == 2)
+				{
+					processRoundRobin(nextOrder);
+				} 
+				else 
+				{
+					processNonPreemptive(nextOrder);
+				}
+			}
+				
+		} catch (InterruptedException e1) 
 		{
 			System.out.println("---Barman is packing up ");
 		}
 	}
-
-	private void executeStandard() throws InterruptedException 
+/**
+ * The processRoundRobin method is responsible for processing a drink order based on the Round Robin (RR) scheduling algorithm.
+ * It takes a DrinkOrder object as a parameter and processes it according to the RR algorithm.
+ * The method first retrieves the remaining time for the order from the remainingTimes map.
+ * If the remaining time is greater than the quantum time, the method simulates working on the order for the quantum time,
+ * updates the remaining time, and re-inserts the order into the order queue with the updated remaining time.
+ * If the remaining time is less than or equal to the quantum time, the method simulates working on the order for the remaining time,
+ * marks the order as done, and removes it from the remainingTimes map.
+ * @param nextOrder The drink order to be processed based on the RR algorithm.
+ */
+	private void processRoundRobin(DrinkOrder nextOrder) throws InterruptedException 
 	{
-		DrinkOrder nextOrder;
-		while (true) 
+		int remainingTime = remainingTimes.get(nextOrder);
+
+        if (remainingTime > quantum) 
 		{
-			nextOrder = orderQueue.take();
-			processOrder(nextOrder);
-		}
+            System.out.println("---Barman preparing part of order for patron " + nextOrder.toString());
+            sleep(quantum);  // Simulate work for 'quantum' milliseconds
+            remainingTime -= quantum;
+            remainingTimes.put(nextOrder, remainingTime);
+            orderQueue.put(nextOrder); // Reinsert with updated remaining time
+        } 
+		else 
+		{
+            System.out.println("---Barman preparing complete order for patron " + nextOrder.toString());
+            sleep(remainingTime); // Processing order
+            System.out.println("---Barman has made order for patron " + nextOrder.toString());
+            nextOrder.orderDone();
+            remainingTimes.remove(nextOrder);
+        }
 	}
 
-	public void processOrder(DrinkOrder order) throws InterruptedException 
+/**
+ * The processNonPreemptive method is responsible for processing a drink order based on the Non-Preemptive (Non-Preemptive) scheduling algorithm.
+ * It takes a DrinkOrder object as a parameter and processes it according to the Non-Preemptive algorithm.
+ * It then simulates working on the order for the order's execution time, and updates the order's status to "done", 
+ * @param nextOrder The drink order to be processed based on the Non-Preemptive algorithm.
+ */
+	private void processNonPreemptive(DrinkOrder nextOrder) throws InterruptedException 
 	{
-		System.out.println("---Barman preparing order for patron "+ order.toString());
-		sleep(order.getExecutionTime()); //processing order
-		System.out.println("---Barman has made order for patron "+ order.toString());
-		order.orderDone();
+		System.out.println("---Barman preparing order for patron " + nextOrder.toString());
+        sleep(nextOrder.getExecutionTime()); // Processing order
+        System.out.println("---Barman has made order for patron " + nextOrder.toString());
+        nextOrder.orderDone();
 	}
 }
 
